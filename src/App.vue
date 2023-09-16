76152ec7666433507471c8b966aa6c0816d1f91a
SystemM1000 <gustaf_alfredo@outlook.com>
2023-09-16 14:57:10 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+// import HelloWorld from './components/HelloWorld.vue'
+import { FoodData } from "./interface/FoodData.ts";
+import { Card } from './components/card/card.vue';
+
+const data: FoodData[] = [];
+
+</script>
+
+<template>
+  <div class="container">
+    <h1>Cardápio</h1>
+    <div class="card-grid">
+      {{ data.map(foodData =>
+        <Card price={ foodData.price } title = { foodData.title } image = { foodData.image } /> ) }}
+    </div>
+  </div>
+  <!--<HelloWorld msg="Vite + Vue" />-->
+</template>
+
+<style scoped></style>
+
+
---
+++ src/components/card/card.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="card">
+    <img :src="image" />
+    <h2>{{ title }}</h2>
+    <p><b>Valor:</b>{{ price }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    price: Number,
+    title: String,
+    image: String
+  }
+}
+</script>
+
+<style scoped>
+.card {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 250px;
+  border-radius: 8px;
+  box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+  padding: 0;
+}
+
+.card img {
+  border-radius: 8px 8px 0 0;
+  width: 250px;
+  height: 200px;
+}
+</style>
